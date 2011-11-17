f100de5837cdc83ee94d67043c992dc084e9c427
Pam Hayes <phayes@umich.edu>
2011-11-17 12:55:22 -0500

    Feature 1:  Add date attribute to SleepEntry class

--- src/main/java/org/sleepfactory/sleeplog/SleepEntry.java
+++ src/main/java/org/sleepfactory/sleeplog/SleepEntry.java
@@ -1,5 +1,28 @@
 package org.sleepfactory.sleeplog;
 
+import java.util.Date;
+
 public class SleepEntry {
+
+	private Date date;
+
+	public SleepEntry() 
+	{
+	}
+
+	public SleepEntry (Date date) 
+	{
+		this.date = date;
+	}
+
+	public Date getDate() 
+	{
+		return date;
+	}
+	
+	public void setDate (Date date) 
+	{
+		this.date = date;
+	}
 
 }
--- src/test/java/org/sleepfactory/sleeplog/SleepEntryTest.java
+++ src/test/java/org/sleepfactory/sleeplog/SleepEntryTest.java
@@ -1,6 +1,8 @@
 package org.sleepfactory.sleeplog;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,9 +10,15 @@ import org.junit.Test;
 public class SleepEntryTest {
 	
 	SleepEntry entry = new SleepEntry();
-
+	SleepEntry entry2;
+	SleepEntry entry3;
+	
 	@Before
 	public void setUp() throws Exception {
+
+		entry = new SleepEntry (new Date());
+		entry2 = new SleepEntry (new Date (entry.getDate().getTime() + 3600000));
+		entry3 = new SleepEntry (entry.getDate());
 	}
 
 	@Test
