cc9ca52ebb4e59b838538c06932c90fb2da17929
Pam Hayes <phayes@umich.edu>
2011-11-17 12:59:31 -0500

    Add restfulness and rested attributes to SleepEntry; set these attributes in that class' test's setUp() method

--- src/main/java/org/sleepfactory/sleeplog/SleepEntry.java
+++ src/main/java/org/sleepfactory/sleeplog/SleepEntry.java
@@ -6,6 +6,9 @@ public class SleepEntry {
 
 	private Date date;
 
+	private Long restedScore;
+	private Long restfulnessScore;
+
 	public SleepEntry() 
 	{
 	}
@@ -23,6 +26,26 @@ public class SleepEntry {
 	public void setDate (Date date) 
 	{
 		this.date = date;
+	}
+
+	public void setRestedScore (Long score) 
+	{
+		this.restedScore = score;
+	}
+
+	public Long getRestedScore() 
+	{
+		return restedScore;
+	}
+
+	public void setResfulnessScore (Long score) 
+	{
+		this.restfulnessScore = score;
+	}
+	
+	public Long getResfulnessScore() 
+	{
+		return restfulnessScore;
 	}
 
 }
--- src/test/java/org/sleepfactory/sleeplog/SleepEntryTest.java
+++ src/test/java/org/sleepfactory/sleeplog/SleepEntryTest.java
@@ -19,6 +19,15 @@ public class SleepEntryTest {
 		entry = new SleepEntry (new Date());
 		entry2 = new SleepEntry (new Date (entry.getDate().getTime() + 3600000));
 		entry3 = new SleepEntry (entry.getDate());
+
+		entry.setRestedScore (4L);
+		entry.setResfulnessScore (4L);
+		
+		entry2.setRestedScore (3L);
+		entry2.setResfulnessScore (4L);
+		
+		entry3.setResfulnessScore (entry.getResfulnessScore());
+		entry3.setRestedScore (entry.getRestedScore());
 	}
 
 	@Test
