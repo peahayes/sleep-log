c865f4b1f50fad6f4521126bf2ebd8a4114049e8
Pam Hayes <phayes@umich.edu>
2011-11-23 14:41:12 -0500

    Start to phase this controller out

--- src/main/java/org/sleepfactory/sleeplog/web/UserController.java
+++ src/main/java/org/sleepfactory/sleeplog/web/UserController.java
@@ -1,13 +1,7 @@
 package org.sleepfactory.sleeplog.web;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import org.sleepfactory.sleeplog.service.SleepService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -23,24 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class UserController {
 	
 	private static final Logger logger = LoggerFactory.getLogger (UserController.class);
-	
-	@Inject @Named ("sleepService")
-	SleepService sleepService;
-
-	@RequestMapping (value = "sleeper/home", method = RequestMethod.GET)
-	public String sleeperHome (Locale locale, Model model) 
-	{
-		logger.info ("Welcome home! the client locale is "+ locale.toString());
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance (DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute ("serverTime", formattedDate );
-		
-		return "secure/sleeper/home";
-	}
 	
 	@RequestMapping (value = "home", method = RequestMethod.GET)
 	public String userHome (Locale locale, Model model) 
