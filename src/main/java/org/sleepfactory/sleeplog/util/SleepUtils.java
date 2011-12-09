a93b6195cbe95502d1d6590d5403a574cd2cffc0
Pam Hayes <phayes@umich.edu>
2011-12-09 13:16:44 -0500

    Add a utils class

---
+++ src/main/java/org/sleepfactory/sleeplog/util/SleepUtils.java
@@ -0,0 +1,25 @@
+package org.sleepfactory.sleeplog.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.sleepfactory.sleeplog.scale.FiveScaleEnum;
+
+public class SleepUtils {
+
+	public static String qualitative (FiveScaleEnum value)
+	{
+		String[] parts = String.valueOf (value).split ("_");
+		for (int i = 0; i < parts.length; i++) firstLetterCaps (parts, i);
+		List<String> list = Arrays.asList (parts);
+		return StringUtils.join (list, ' ');
+	}
+
+	private static void firstLetterCaps (String parts[], int i) 
+	{
+		parts[i] = parts[i].toLowerCase();
+		parts[i] = StringUtils.capitalize (parts[i]);
+	}
+
+}
