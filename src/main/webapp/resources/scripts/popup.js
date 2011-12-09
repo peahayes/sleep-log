5172ce40f8da339bf7f9e5540083e29f31065bbd
Pam Hayes <phayes@umich.edu>
2011-12-09 15:01:00 -0500

    add a style for our popup iframe

--- src/main/webapp/resources/scripts/popup.js
+++ src/main/webapp/resources/scripts/popup.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
  */
 function initializeAddEditPopUp() {
 	//Examples of how to assign the ColorBox event to elements
+	$(".editFrame").colorbox({width:"65%", height:"50%", iframe:true, scrolling: true});
 	$(".example5").colorbox({width:"50%", height:"60%", inline:true, href:"#inline_example4", scrolling: false });
 	$(".example6").colorbox({width:"50%", height:"75%", iframe:true, scrolling: true});
 	$(".example2").colorbox({width:"50%", height:"75%", iframe:true, scrolling: true});
