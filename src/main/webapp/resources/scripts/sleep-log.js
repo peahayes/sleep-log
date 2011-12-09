6968468187f6d70aeb2f7646a0b5d2935fbeaa71
Pam Hayes <phayes@umich.edu>
2011-12-09 09:04:35 -0500

    Add function processDeleteClick()

--- src/main/webapp/resources/scripts/sleep-log.js
+++ src/main/webapp/resources/scripts/sleep-log.js
@@ -12,3 +12,11 @@ function setDocVars (wStyle)
 {
 	popupWindowStyle = wStyle;
 }
+
+function processDeleteClick (id, action)
+{
+	if (confirm ("Are you sure you want to delete?"))
+		window.location = action + "?id=" + id;
+	else
+		return false;
+}
