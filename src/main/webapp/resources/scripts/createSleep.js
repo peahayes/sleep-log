33ce65e8defe4968fc4f7a707da57b99dddb7b34
Pam Hayes <phayes@umich.edu>
2011-12-14 14:21:24 -0500

    Add function showRestulness()

--- src/main/webapp/resources/scripts/createSleep.js
+++ src/main/webapp/resources/scripts/createSleep.js
@@ -5,3 +5,12 @@ $(document).ready (function()
 	displayEnergyLevelsForURL ("../../rest/getEnergyLevels");
 	displayEnergyLevelsForURL ("../rest/getEnergyLevels");
 });
+
+function showRestulness()
+{
+	// only one of these will succeed, depending on whether we are creating 
+	// a new entry or updating an existing entry
+	displayRestulnessScoresForURL ("../../rest/getRestfulnessScores");
+	displayRestulnessScoresForURL ("../rest/getRestfulnessScores");
+	$("#restfulRow").show();
+}
