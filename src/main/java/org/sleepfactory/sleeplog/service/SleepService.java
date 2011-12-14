352fbe0273bf8232c33fef0479905ae91fec7106
Pam Hayes <phayes@umich.edu>
2011-12-14 14:20:07 -0500

    Add method getRestfulnessScores(), and add some Thread.sleeps() to this and getEnergyLevels() to simulate an actual server request taking a little time to complete, since these are called via Ajax

--- src/main/java/org/sleepfactory/sleeplog/service/SleepService.java
+++ src/main/java/org/sleepfactory/sleeplog/service/SleepService.java
@@ -7,14 +7,18 @@ import java.util.Set;
 import org.sleepfactory.sleeplog.SleepEntry;
 import org.sleepfactory.sleeplog.SleepLog;
 import org.sleepfactory.sleeplog.scale.EnergyLevel;
+import org.sleepfactory.sleeplog.scale.Restfulness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service ("sleepService")
 public class SleepService {
 	
 	private SleepLog log = new SleepLog();
-	
 	private Long masterId = 1L;
+
+	private static final Logger logger = LoggerFactory.getLogger (SleepService.class);
 
 	public Set<SleepEntry> getEntries()
 	{
@@ -60,29 +64,50 @@ public class SleepService {
 		log.getEntries().remove (entry);
 	}
 
-	public List<Energy> getEnergyLevels() 
+	public List<Score> getEnergyLevels() 
+		throws InterruptedException
 	{
-		List<Energy> levels = new ArrayList<Energy>();
+		List<Score> levels = new ArrayList<Score>();
 		
-		levels.add (new Energy (EnergyLevel.EXTREMELY_FATIGUED.valueOf(), EnergyLevel.EXTREMELY_FATIGUED.qualitative()));
-		levels.add (new Energy (EnergyLevel.MODERATELY_FATIGUED.valueOf(), EnergyLevel.MODERATELY_FATIGUED.qualitative()));
-		levels.add (new Energy (EnergyLevel.MILDLY_FATIGUED.valueOf(), EnergyLevel.MILDLY_FATIGUED.qualitative()));
-		levels.add (new Energy (EnergyLevel.SOMEWHAT_ENERGETIC.valueOf(), EnergyLevel.SOMEWHAT_ENERGETIC.qualitative()));
-		levels.add (new Energy (EnergyLevel.VERY_ENERGETIC.valueOf(), EnergyLevel.VERY_ENERGETIC.qualitative()));
+		levels.add (new Score (EnergyLevel.EXTREMELY_FATIGUED.valueOf(), EnergyLevel.EXTREMELY_FATIGUED.qualitative()));
+		levels.add (new Score (EnergyLevel.MODERATELY_FATIGUED.valueOf(), EnergyLevel.MODERATELY_FATIGUED.qualitative()));
+		levels.add (new Score (EnergyLevel.MILDLY_FATIGUED.valueOf(), EnergyLevel.MILDLY_FATIGUED.qualitative()));
+		levels.add (new Score (EnergyLevel.SOMEWHAT_ENERGETIC.valueOf(), EnergyLevel.SOMEWHAT_ENERGETIC.qualitative()));
+		levels.add (new Score (EnergyLevel.VERY_ENERGETIC.valueOf(), EnergyLevel.VERY_ENERGETIC.qualitative()));
+		
+		logger.info ("Sleeping for 800 milliseconds");
+		Thread.sleep (800);
 		
 		return levels;
 	}
 	
-	public class Energy 
+	public List<Score> getRestfulnessScores() 
+		throws InterruptedException
+	{
+		List<Score> levels = new ArrayList<Score>();
+		
+		levels.add (new Score (Restfulness.NOT_AT_ALL.valueOf(), Restfulness.NOT_AT_ALL.qualitative()));
+		levels.add (new Score (Restfulness.SLIGHTLY.valueOf(), Restfulness.SLIGHTLY.qualitative()));
+		levels.add (new Score (Restfulness.SOMEWHAT.valueOf(), Restfulness.SOMEWHAT.qualitative()));
+		levels.add (new Score (Restfulness.RESTED.valueOf(), Restfulness.RESTED.qualitative()));
+		levels.add (new Score (Restfulness.WELL_RESTED.valueOf(), Restfulness.WELL_RESTED.qualitative()));
+		
+		logger.info ("Sleeping for 800 milliseconds");
+		Thread.sleep (800);
+		
+		return levels;
+	}
+
+	public class Score 
 	{
 		public String description;
 		public String value;
 		
-		public Energy (Long val, String desc)
+		public Score (Long val, String desc)
 		{
 			this.description = desc;
 			this.value = String.valueOf (val);
 		}
 	}
-
+	
 }
